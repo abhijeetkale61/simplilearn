5b7c14a4759374421eb957b20222e48bbe21c52e
abhijeetkale61 <99884251+abhijeetkale61@users.noreply.github.com>
2022-03-14 15:45:40 +0530

    Add files via upload

---
+++ Map/src/assistedproject/Main.java
@@ -0,0 +1,41 @@
+package assistedproject;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		HashMap<Integer,String> hm=new HashMap<Integer,String>();      
+	      hm.put(1,"abhi");    
+	      hm.put(2,"ajay");    
+	      hm.put(3,"amol");   
+	       
+	      System.out.println("\nThe elements of Hashmap are ");  
+	      for(Map.Entry m:hm.entrySet()){    
+	       System.out.println(m.getKey()+" "+m.getValue());    
+	      }
+	     
+	       
+	      Hashtable<Integer,String> ht=new Hashtable<Integer,String>();  
+	      
+	      ht.put(4,"Akash");  
+	      ht.put(5,"Ram");  
+	      ht.put(6,"ravan");  
+	      ht.put(7,"pritish");  
+	      System.out.println("\nThe elements of HashTable are ");  
+	      for(Map.Entry n:ht.entrySet()){    
+	       System.out.println(n.getKey()+" "+n.getValue());    
+	      }
+	      
+	      
+	      
+	      
+	      TreeMap<Integer,String> map=new TreeMap<Integer,String>();    
+	      map.put(8,"Anand");    
+	      map.put(9,"sachin");    
+	      map.put(10,"makrand");       
+	      
+	      System.out.println("\nThe elements of TreeMap are ");  
+	      for(Map.Entry l:map.entrySet()){    
+	       System.out.println(l.getKey()+" "+l.getValue());    
+	      }    
+
+	}
+}
