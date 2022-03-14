0ab0396058162206dbbb166a6567f3fc6cff4ec2
abhijeetkale61 <99884251+abhijeetkale61@users.noreply.github.com>
2022-03-14 15:24:43 +0530

    Add files via upload

---
+++ bubblesort/Array/src/assistedproject/arraydemo.java
@@ -0,0 +1,20 @@
+package assistedproject;
+
+public class arraydemo {
+	public static void main(String[] args) {
+
+		
+		int a[]= {60,20,30,40,50};
+		for(int i=0;i<5;i++) {
+			System.out.println("Elements of array a: "+a[i]);
+			}
+
+			int[][] b = {
+			            {5, 4, 6, 8}, 
+			            {8, 6, 9} };
+			      
+			      System.out.println("\nLength of row 1: " + b[0].length);
+			      }
+			}
+
+
