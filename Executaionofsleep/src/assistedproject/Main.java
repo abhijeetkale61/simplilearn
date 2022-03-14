78baef5db0198df41cdc2cbd53115efde34b07ab
abhijeetkale61 <99884251+abhijeetkale61@users.noreply.github.com>
2022-03-14 15:36:38 +0530

    Add files via upload

---
+++ Executaionofsleep/src/assistedproject/Main.java
@@ -0,0 +1,18 @@
+package assistedproject;
+
+public class Main {
+	
+	    private static Object LOCK = new Object();
+	    public static void main(String args[]) throws InterruptedException
+	    {
+	        Thread.sleep(1000);
+	        System.out.println("Thread '" + Thread.currentThread().getName() + "' is woken after sleeping for 30 second");
+	        synchronized (LOCK) 
+	        {
+	            LOCK.wait(1000);
+	            System.out.println("Object '" + LOCK + "' is woken after" + " waiting for 20 second");
+	        }
+	    }
+	}
+
+
